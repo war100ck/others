2eef527d3a319ef74ecc321ed0c8c7ecf28c922b
WAR100CK <war100ck2008@yandex.ru>
2020-04-23 10:27:33 +0300

    v92.4

--- Gathering-RU/index.js
+++ Gathering-RU/index.js
@@ -1,83 +1,85 @@
 module.exports = function Gathering(mod) {
-	const Message = require('../tera-message')
-	const MSG = new Message(mod)
-	
-	if (mod.proxyAuthor !== 'caali') {
-		const options = require('./module').options
-		if (options) {
-			const settingsVersion = options.settingsVersion
-			if (settingsVersion) {
-				mod.settings = require('./' + (options.settingsMigrator || 'settings_migrator.js'))(mod.settings._version, settingsVersion, mod.settings)
-				mod.settings._version = settingsVersion
-			}
-		}
-	}
-	
 	let plantsMarkers = false,
 		miningMarkers = false,
-		energyMarkers = false
+		energyMarkers = false,
+		plants = false,
+		mining = false,
+		energy = false
 	
 	let mobid = [],
 		gatherMarker = []
 	
-	function СборStatus() {
+	function gatheringStatus() {
 		sendStatus(
-			"Сбор: " + (mod.settings.enabled ? MSG.BLU("Включен") : MSG.YEL("Выключен")),
-			"Предупреждающее сообщение: " + (mod.settings.sendToAlert  ? MSG.BLU("Включен") : MSG.YEL("Выключен")),
-			"Уведомление: " + (mod.settings.sendToNotice ? MSG.BLU("Включен") : MSG.YEL("Выключен")),
+			"Сбор: "+ (mod.settings.enabled      ? "Включен"   : "Off"),
+			"Предупреждающее сообщение: " + (mod.settings.sendToAlert  ? "Включен" : "Выключен"),
 			
-			"Подсказки по растениям: " + (plantsMarkers ? MSG.BLU("Включен") : MSG.YEL("Выключен")),
-			"Подсказки по руде: " + (miningMarkers ? MSG.BLU("Включен") : MSG.YEL("Выключен")),
-			"Подсказки по эссенции: " + (energyMarkers ? MSG.BLU("Включен") : MSG.YEL("Выключен"))
+			"Подсказки по растениям: " + (plantsMarkers ? "Включен" : "Выключен"),
+			"Подсказки по руде: " + (miningMarkers ? "Включен" : "Выключен"),
+			"Подсказки по эссенции: " + (energyMarkers ? "Включен" : "Выключен"),
+			"Растение: "     + (plants        ? "Включен" : "Выключен"),
+			"Руда: "     + (mining        ? "Включен" : "Выключен"),
+			"Эссенция: "     + (energy        ? "Включен" : "Выключен"),
 		)
 	}
 	
 	function sendStatus(msg) {
-		MSG.chat([...arguments].join('\n\t - '))
+		sendMessage([...arguments].join('\n\t - '))
 	}
 	
 	mod.command.add("сбор", (arg) => {
 		if (!arg) {
-			mod.settings.enabled = !mod.settings.enabled;
+			mod.settings.enabled = !mod.settings.enabled
 			if (!mod.settings.enabled) {
 				plantsMarkers = false
 				miningMarkers = false
 				energyMarkers = false
+				plants = false
+				mining = false
+				energy = false
 				for (let itemId of mobid) {
 					despawnItem(itemId)
 				}
 			}
-			MSG.chat("Сбор " + (mod.settings.enabled ? MSG.BLU("Включен") : MSG.YEL("Выключен")))
+			gatheringStatus()
 		} else {
 			switch (arg) {
 				case "warn":
 					mod.settings.sendToAlert = !mod.settings.sendToAlert
-					MSG.chat("Предупреждающее сообщение " + (mod.settings.sendToAlert ? MSG.BLU("Включен") : MSG.YEL("Выключен")))
+					sendMessage("Предупреждающее сообщение " + (mod.settings.sendToAlert ? "Включен" : "Выключен"))
 					break
-				case "not":
-					mod.settings.sendToNotice = !mod.settings.sendToNotice
-					MSG.chat("Уведомление " + (mod.settings.sendToNotice ? MSG.BLU("Включен") : MSG.YEL("Выключен")))
-					break
-					
 				case "status":
-					СборStatus()
+					gatheringStatus()
 					break
 				
 				case "растение":
 					plantsMarkers = !plantsMarkers
-					MSG.chat("Подсказки по растениям -  " + (plantsMarkers ? MSG.BLU("Показаны") : MSG.YEL("Скрыты")))
+					sendMessage("Подсказки по растениям -  " + (plantsMarkers ? "Показаны" : "Скрыты"))
 					break
 				case "руда":
 					miningMarkers = !miningMarkers
-					MSG.chat("Подсказки по руде -  " + (miningMarkers ? MSG.BLU("Показаны") : MSG.YEL("Скрыты")))
+					sendMessage("Подсказки по руде -  " + (miningMarkers ? "Показаны" : "Скрыты"))
 					break
 				case "эссенция":
 					energyMarkers = !energyMarkers
-					MSG.chat("Подсказки по эссенции - " + (energyMarkers ? MSG.BLU("Показаны") : MSG.YEL("Скрыты")))
+					sendMessage("Подсказки по эссенции - " + (energyMarkers ? "Показаны" : "Скрыты"))
+					break
+				
+				case "кустарник":
+					plants = !plants
+					sendMessage("Подсказки по Густому кустарнику (Редкий) - " + (plants ? "Показаны" : "Скрыты"))
+					break
+				case "валун":
+					mining = !mining
+					sendMessage("Подсказки по Валуну (Редкий) - " + (mining ? "Показаны" : "Скрыты"))
+					break
+				case "кристалл":
+					energy = !energy
+					sendMessage("Подсказки по Бесцветному кристаллу (Редкий) - " + (energy ? "Показаны" : "Скрыты"))
 					break
 				
 				default :
-					MSG.chat("Сбор " + MSG.RED("Неверный параметр!"))
+					sendMessage("Неверный параметр!!")
 					break
 			}
 		}
@@ -90,14 +92,23 @@ module.exports = function Gathering(mod) {
 	mod.hook('S_SPAWN_COLLECTION', 4, (event) => {
 		if (mod.settings.enabled) {
 			if (plantsMarkers && (gatherMarker = mod.settings.plants.find(obj => obj.id === event.id))) {
-				MSG.alert(("Обнаружено [" + gatherMarker.name + "] " + gatherMarker.msg), 44)
-				MSG.raids( "Обнаружено [" + gatherMarker.name + "] " + gatherMarker.msg)
+				sendAlert( ("Обнаружено [" + gatherMarker.name + "] " + gatherMarker.msg), 44)
+				sendMessage("Обнаружено [" + gatherMarker.name + "] " + gatherMarker.msg)
 			} else if (miningMarkers && (gatherMarker = mod.settings.mining.find(obj => obj.id === event.id))) {
-				MSG.alert(("Обнаружено [" + gatherMarker.name + "] " + gatherMarker.msg), 44)
-				MSG.raids( "Обнаружено [" + gatherMarker.name + "] " + gatherMarker.msg)
+				sendAlert( ("Обнаружено [" + gatherMarker.name + "] " + gatherMarker.msg), 44)
+				sendMessage("Обнаружено [" + gatherMarker.name + "] " + gatherMarker.msg)
 			} else if (energyMarkers && (gatherMarker = mod.settings.energy.find(obj => obj.id === event.id))) {
-				MSG.alert(("Обнаружено [" + gatherMarker.name + "] " + gatherMarker.msg), 44)
-				MSG.raids( "Обнаружено [" + gatherMarker.name + "] " + gatherMarker.msg)
+				sendAlert( ("Обнаружено [" + gatherMarker.name + "] " + gatherMarker.msg), 44)
+				sendMessage("Обнаружено [" + gatherMarker.name + "] " + gatherMarker.msg)
+			} else if (plants && event.id == 1) {
+				sendAlert( ("Обнаружен [Редкий Густой кустарник] "), 44)
+				sendMessage("Обнаружено [Редкий Густой кустарник] ")
+			} else if (mining && event.id == 101) {
+				sendAlert( ("Обнаружен [Редкий Валун] "), 44)
+				sendMessage("Обнаружен [Валун] ")
+			} else if (energy && event.id == 201) {
+				sendAlert( ("Обнаружен [Редкий Бесцветный кристалл] "), 44)
+				sendMessage("Обнаружен [Редкий Бесцветный кристалл] ")
 			} else {
 				return true
 			}
@@ -128,8 +139,18 @@ module.exports = function Gathering(mod) {
 	
 	function despawnItem(gameId) {
 		mod.send('S_DESPAWN_DROPITEM', 4, {
-			gameId: gameId*100n
+			gameId: gameId*10n
 		})
 	}
 	
+	function sendMessage(msg) { mod.command.message(msg) }
+	
+	function sendAlert(msg, type) {
+		mod.send('S_DUNGEON_EVENT_MESSAGE', 2, {
+			type: type,
+			chat: false,
+			channel: 0,
+			message: msg,
+		})
+	}
 }
